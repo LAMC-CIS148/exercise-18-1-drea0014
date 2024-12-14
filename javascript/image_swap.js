7a0435229eda54d5dc7ce9747c57947ac66a57d0
drea0014 <dreyah0014@gmail.com>
2024-12-14 15:22:54 -0800

    Update image_swap.js

--- javascript/image_swap.js
+++ javascript/image_swap.js
@@ -1,20 +1,23 @@
 "use strict";
-
 $(document).ready(function() {
+    // Update the year in the footer
+    const currentYear = new Date().getFullYear();
+    $("footer p").html(`&copy; ${currentYear}, San Joaquin Valley Town Hall, Fresno, CA 93755`);
+    
     // Preload images
     $("#image_list a").each(function() {
-        var swappedImage = new Image();
+        const swappedImage = new Image();
         swappedImage.src = $(this).attr("href");
     });
 
-    // Set up event handlers for links
+    // Set up event handlers for links    
     $("#image_list a").click(function(evt) {
         // Swap image
-        var imageURL = $(this).attr("href");
+        const imageURL = $(this).attr("href");
         $("#image").attr("src", imageURL);
 
         // Swap caption
-        var caption = $(this).attr("title");
+        const caption = $(this).attr("title");
         $("#caption").text(caption);
 
         // Cancel the default action of the link
@@ -22,5 +25,6 @@ $(document).ready(function() {
     });
 
     // Move focus to the first thumbnail
-    $("#image_list li:first-child a").focus();
+    $("li:first-child a:first-child").focus();
 });
+
